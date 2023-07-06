0cb40d55377ef8d3318998f7e11b6814b636f274
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-07-06 16:24:42 +0300

    03-changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,29 +4,27 @@ const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', _.throttle(handlerInput, 500));
 
-const data = {};
+let data = JSON.parse(localStorage.getItem('feedback-form-state') || '{}');
 
 function handlerInput(evt) {
   data[evt.target.name] = evt.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
-const dataSaver = localStorage.getItem('feedback-form-state');
-
 window.addEventListener('load', handlerLoad);
 
 function handlerLoad() {
-  let data = JSON.parse(localStorage.getItem('feedback-form-state') || '{}');
   Object.keys(data).forEach(name => {
     const inp = form.querySelector(`[name="${name}"]`);
     if (inp) inp.value = data[name];
-  }); 
-  console.log();
+  });
 }
 
 form.addEventListener('submit', handlerSubmit);
 
-function handlerSubmit() {
-  console.log(localStorage.getItem('feedback-form-state'));
+function handlerSubmit(event) {
+  event.preventDefault();
+  console.log(data);
   localStorage.removeItem('feedback-form-state');
+  data = {};
 }
