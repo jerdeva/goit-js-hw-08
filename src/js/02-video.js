a25c2b0d64baa784b95eb60ffd782dfe82c07c84
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-07-06 10:21:46 +0300

    HW - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -28,16 +28,13 @@ console.log(LS);
 player
   .setCurrentTime(LS)
   .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
   })
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-        // some other error occurred
         break;
     }
   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import * as _ from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', _.throttle(handlerInput, 500));
+
+const data = {};
+
+function handlerInput(evt) {
+  data[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+const dataSaver = localStorage.getItem('feedback-form-state');
+
+window.addEventListener('load', handlerLoad);
+
+function handlerLoad() {
+  let data = JSON.parse(localStorage.getItem('feedback-form-state') || '{}');
+  Object.keys(data).forEach(name => {
+    const inp = form.querySelector(`[name="${name}"]`);
+    if (inp) inp.value = data[name];
+  }); 
+  console.log();
+}
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit() {
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+}
